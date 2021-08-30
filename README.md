3a4ecb23c20b9e936ece8c03970ca349878a972a
fariz <farizi1906@gmail.com>
2021-08-30 16:09:01 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chat-app
+
+a simple chat website build using python flask,socketio and mysql as database
---
+++ wsgi.py
@@ -0,0 +1,85 @@
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    session,
+    redirect,
+    url_for,
+    flash,
+)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_socketio import SocketIO, send
+
+from sqlalchemy.sql import func
+
+from functools import wraps
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+db = SQLAlchemy(app, session_options={"autoflush": False})
+migrate = Migrate(app, db)
+
+app.config["SECRET_KEY"] = os.urandom(128)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql+pymysql://root:f%40r1Zi1906@localhost:3306/chat"
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(24), nullable=False)
+    message = db.Column(db.String(2000), nullable=False)
+    date = db.Column(db.String(2000))
+
+
+@app.route("/chat_room")
+def chat_room():
+    print(session)
+    if "logged" in session and "username" in session:
+        messages = Message.query.all()
+        return render_template("chat_room.html", messages=messages)
+    else:
+        return redirect(url_for("register"))
+
+
+@app.route("/", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        session["logged"] = True
+        session["username"] = request.form.get("username")
+        return redirect(url_for("chat_room"))
+    if request.method == "GET":
+        if "logged" in session and "username" in session:
+            return redirect(url_for("chat_room"))
+        else:
+            return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("register"))
+
+
+def save_message(sender, message, date):
+
+    add_message = Message(username=sender, message=message, date=date)
+    db.session.add(add_message)
+    db.session.commit()
+
+
+@socketio.on('message')
+def handleMessage(msg):
+    import json
+
+    response = json.loads(msg)
+    save_message(response["sender"], response["msg"], response["date"])
+    send(msg, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
