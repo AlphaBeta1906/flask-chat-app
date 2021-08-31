031a890da2027a21b3313eb7de844a76f23e35ad
fariz <farizi1906@gmail.com>
2021-08-31 16:43:08 +0700

    add simple markdown support and update ui

--- wsgi.py
+++ wsgi.py
@@ -22,13 +22,11 @@ db = SQLAlchemy(app, session_options={"autoflush": False})
 migrate = Migrate(app, db)
 
 app.config["SECRET_KEY"] = os.urandom(128)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://root:f%40r1Zi1906@localhost:3306/chat"
+app.config["SQLALCHEMY_DATABASE_URI"] = "your_database_url"
 app.config["SQLALCHEMY_POOL_TIMEOUT"] = 86400
 app.config["SQLALCHEMY_POOL_SIZE"] = 200
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 100
-app.config["ENV"] = "production"
+app.config["ENV"] = "development"
 
 
 class Message(db.Model):
@@ -41,7 +39,10 @@ class Message(db.Model):
 @app.template_filter("render_content")
 def render(content: str):
     return markdown.markdown(
-        content, safe_mode=True, enable_attributes=False, extensions=["pymdownx.magiclink", "pymdownx.tilde"]
+        content,
+        safe_mode=True,
+        enable_attributes=False,
+        extensions=["pymdownx.magiclink", "pymdownx.tilde"],
     )
 
 
