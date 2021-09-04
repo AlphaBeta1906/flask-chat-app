1b7bfc6e75f71d1809e073ad0e8e07763bfde327
fariz <farizi1906@gmail.com>
2021-09-04 17:53:14 +0700

    now use jsx to render message

--- static/index.js
+++ static/index.js
@@ -1,16 +1,50 @@
+	hljs.initHighlightingOnLoad();
+			function render_markdown(text){
+				return marked(text.replace("&gt;", '>'),{
+        		sanitize : true,
+        		smartList : true,
+        		highlight: function(code, lang) {
+            			const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            			return hljs.highlight(code, { language }).value;
+        			}
+        		})
+			}
 		var socket = io.connect()
 		socket.on('message',function(response){
 			var parse_response = JSON.parse(response)
+			$('#msg').append('<li class="card rounded shadow my-5 message"></li>');
+			const messages = document.getElementsByTagName('li');
+			const latestMessage = messages[messages.length - 1]
+
+
+			ReactDOM.render(
+						<div class="container-fluid">
+							<p class="fw-bold">{parse_response.sender} : </p> 
+							<p id="message-body"  
+							dangerouslySetInnerHTML={{ __html: render_markdown(parse_response.msg)  }}></p>
+							<p>{parse_response.date}</p>
+						</div>,
+  					latestMessage
+			);
+			$("code").each(function(){
+		    	if ($(this).length && $(this).parents("pre").length == 1 ){
+	        		$(this).addClass('hljs')
+	    		}
+	    	})
+			$("message-body").html($("message-body").text())
+			/*
 			$('#msg').append(
 			'<li id="new_message"  class="card rounded shadow my-5">' +
 				'<div class = "container-fluid">'+
 					'<p>' + '<span class="fw-bold">' + parse_response["sender"] + '</span>'  + ' : '
-					+ marked(parse_response["msg"]) + '</p>' +
+					+ render_markdown(parse_response["msg"]) + '</p>' +
 					'<p>'+
 					parse_response["date"]+'</p>' +
 				'</div> </li>').show();
+				*/
 				$("#msg").scrollTop($("#msg")[0].scrollHeight);
 				$("#none").remove()
+
 					}
 				);
 					$('#send').on('click',function(){
@@ -31,7 +65,7 @@
 							var message_string = JSON.stringify({
 													sender: $("#username").html(),
 													msg: $("#message").val(),
-													date:year + "/" + month + "/" + date + " " + hours + ":" + minutes + ":" + Math.round(seconds)
+													date:year + "/" + month + "/" + date + " " + hours + ":" + minutes + ":" + Math.round(seconds),
 												})
 							if ($("#sender").val() === "" || $("#message").val() === "") {
 								alert("empty value")
@@ -47,9 +81,25 @@
 					if(event.keyCode == 13){
 					console.log("s")
 					}
+
 				});
-				/*
-				$(window).on('load', function() {
-					$("#msg").scrollTop($("#msg")[1].scrollHeight);
-				});
-				*/
+
+				$('*[id*=message-body]:visible').each(function() {
+    					console.log($("#message-body").text())
+	    			$(this).html(render_markdown($(this).text()))			
+	    		});
+
+	    			$("code").each(function(){
+		    			if ($(this).length && $(this).parents("pre").length == 1 ){
+	        				$(this).addClass('hljs')
+	    				}
+	    			})
+	$('#message-body').html(marked($('#message-body').text().replace("&gt;", '>'),{
+	sanitize : true,
+	smartList : true,
+	highlight: function(code, lang) {
+	const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+	return hljs.highlight(code, { language }).value;
+	},
+	}))
+
